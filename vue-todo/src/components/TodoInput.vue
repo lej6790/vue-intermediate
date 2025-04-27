ffe240dd3ddb2ab89497be7ded8f01400bc5b8ae
wonjong <lej6790@gmail.com>
2025-04-27 18:02:24 +0900

    Implement TodoInput function

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,12 +1,23 @@
 <template>
     <div>
-        input
+        <input type="text" v-model="newTodoItem">
+        <button v-on:click="addTodo">add</button>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            newTodoItem: ""
+        };
+    },
+    methods: {
+        addTodo() {
+            localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.newTodoItem = '';
+        }
+    }
 }
 </script>
 
