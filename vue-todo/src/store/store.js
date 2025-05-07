3ae53427fbdf4c7828d142de8600d7e9d7ea0257
wonjong <lej6790@gmail.com>
2025-05-07 23:51:46 +0900

    [리팩토링] mutations 적용

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -5,7 +5,7 @@ const storage = {
         const arr = [];
         if (localStorage.length > 0) {
             for (let i = 0; i < localStorage.length; i++) {
-                if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+                if (localStorage.key(i) !== '__VUE_DEVTOOLS_KIT_TIMELINE_LAYERS_STATE__') {
                     arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
         
                 }
@@ -20,5 +20,25 @@ export const store = createStore({
         return {
             todoItems: storage.fetch()
         };
+    },
+    mutations: {
+        addOneItem(state, todoItem) {
+            const obj = {completed: false, item: todoItem};
+            localStorage.setItem(todoItem, JSON.stringify(obj));
+            state.todoItems.push(obj);
+        },
+        removeOneItem(state, payload) {
+            localStorage.removeItem(payload.todoItem.item);
+            state.todoItems.splice(payload.index, 1);
+        },
+        toggleOneItem(state, payload) {
+            state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
+            localStorage.removeItem(payload.todoItem.item);
+            localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
+        },
+        clearAllItems(state) {
+            localStorage.clear();
+            state.todoItems = [];
+        }
     }
 });
